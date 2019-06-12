b8d770313e4cec55f21d0410b29cb2f3abce26e7
杨乐 <yangle06@58ganji.com>
2019-06-12 21:45:28 +0800

    0612work

--- app/src/main/java/com/example/yangl/androidsample/activity/GlideDemoActivity.java
+++ app/src/main/java/com/example/yangl/androidsample/activity/GlideDemoActivity.java
@@ -1,5 +1,6 @@
 package com.example.yangl.androidsample.activity;
 
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,8 +13,10 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.ViewTarget;
 import com.example.yangl.androidsample.R;
 import com.example.yangl.androidsample.fragment.EmptyFragment;
+import com.example.yangl.androidsample.tools.UrlImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,7 +27,7 @@ public class GlideDemoActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.image_view)
-    ImageView imageView;
+    UrlImageView imageView;
     @BindView(R.id.fab)
     FloatingActionButton fab;
 
@@ -49,10 +52,12 @@ public class GlideDemoActivity extends AppCompatActivity {
 
     private void initImageView(){
         String imageUrl = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560273719736&di=023e517182e739037b3b007d315eb455&imgtype=0&src=http%3A%2F%2Fk.zol-img.com.cn%2Fsjbbs%2F7692%2Fa7691515_s.jpg";
-        Glide.with(this)
-                .load(Uri.parse(imageUrl))
-                .apply(new RequestOptions().error(R.drawable.icon1))
-                .into(imageView);
+//        Glide.with(this)
+//                .load(Uri.parse(imageUrl))
+//                .apply(new RequestOptions().error(R.drawable.icon1))
+//                .into(imageView);
+
+        imageView.setImageURL(imageUrl);
 
     }
 
