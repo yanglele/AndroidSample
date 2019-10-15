8b3ff10502e34541277ac5046ddc5eff5beb7821
杨乐 <yangle06@58ganji.com>
2019-10-15 19:49:33 +0800

    t2

--- algo/src/main/java/com/example/algo/ThreadPoolClass.java
+++ algo/src/main/java/com/example/algo/ThreadPoolClass.java
@@ -10,6 +10,7 @@ public class ThreadPoolClass {
     public static void main(String[] args) throws InterruptedException {
         newCachedThreadPoolTest();
         int a=0;
+        int b=2;
     }
 
 
